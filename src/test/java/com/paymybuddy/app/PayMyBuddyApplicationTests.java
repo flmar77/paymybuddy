5e07b60a7c34da2015f7f176efd823212c57970b
SIEGE\S637606 <florian.marguerite@axa.fr>
2022-04-04 09:08:45 +0200

    init

---
+++ src/test/java/com/paymybuddy/app/PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
