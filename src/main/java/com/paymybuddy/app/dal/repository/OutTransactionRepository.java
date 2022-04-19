0929149b525615408a558ef4c28f31ebc44f8819
SIEGE\S637606 <florian.marguerite@axa.fr>
2022-04-19 09:47:27 +0200

    IntegrationTestsCompleted

--- src/main/java/com/paymybuddy/app/dal/repository/OutTransactionRepository.java
+++ src/main/java/com/paymybuddy/app/dal/repository/OutTransactionRepository.java
@@ -4,6 +4,9 @@ import com.paymybuddy.app.dal.entity.OutTransactionEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OutTransactionRepository extends CrudRepository<OutTransactionEntity, Integer> {
+    List<OutTransactionEntity> findByUserId(Integer id);
 }
